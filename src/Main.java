b7d48d39d4065f72288a2e2bcf68f6b9a8552d65
Gabi <gabita.lucutar@yahoo.com>
2021-12-07 17:51:00 +0200

    class user complete

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+import domain.User;
+
+public class Main {
+    public static void main(String[] args) {
+        User user = new User("vlad01", "Luca", "Vlad");
+        System.out.println(user);
+
+    }
+}
--- src/domain/User.java
+++ src/domain/User.java
@@ -1,4 +1,40 @@
 package domain;
 
-public class User {
+public class User extends Entity<String>{
+    private String firstName;
+    private String lastName;
+
+    public User(String username, String firstName, String lastName) {
+        setUsername(username);
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+
+    @Override
+    public String toString() {
+
+        return "Utilizator{" +
+                "username=" + getUsername() +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' + " }";
+
+    }
+
 }
